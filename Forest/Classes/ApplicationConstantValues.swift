71c08ca4f06bd7a68601e69a31ef8aa5332a6cb3
Sitdhibong Laokok <goto.champ@gmail.com>
2014-08-23 12:35:30 +0700

    Constant value

---
+++ Forest/Classes/ApplicationConstantValues.swift
@@ -0,0 +1,15 @@
+//
+//  ApplicationConstantValues.swift
+//  Forest
+//
+//  Created by Sitdhibong Laokok on 23/8/57.
+//  Copyright (c) พ.ศ. 2557 App Tree. All rights reserved.
+//
+
+import Foundation
+
+let kGoogleAppIdentifier    = "MY_GOOGLE_APPLICATION_IDENTIFIER"
+let kGoogleAppSecret        = "MY_GOOGLE_APPLICATION_SECRET_TOKEN"
+
+let kFacebookAppIdentifier  = "MY_FACEBOOK_APPLICATION_IDENTIFIER"
+let kFacebookAppSecret      = "MY_FACEBOOK_APPLICATION_SECRET_TOKEN"
